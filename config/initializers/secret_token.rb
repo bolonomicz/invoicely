cfe4382235973ec65e94778f114c3d7eede54d5a
Ralph <ralph.bolo@gmail.com>
2014-05-24 00:33:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Invoicely::Application.config.secret_key_base = '91a87f3215761a302d75e6ddfb113f492a92606f550d14f03f410dde70f539c3dcafdcfafa289e87a644576f7d15fd7d6ee0d394703b16f618582b7003eb85f8'
